04162c883d99dc55758075b070ad6bc62f82f769
Alexandra2888 <moldovan.alexandra28@gmail.com>
2022-04-16 23:13:06 +0300

    updating project

--- server.js
+++ server.js
@@ -13,11 +13,10 @@ const db = knex({
 
   client: 'pg',
   connection: {
-   connectionString : 'ec2-23-20-224-166.compute-1.amazonaws.com',
-    user : 'gjbpyqscsqrckq',
+    host : '127.0.0.1',
+    user : 'postgres',
     password : 'Alexandra',
-    database : 'dems4kfejecjq7',
-    ssl: true
+    database : 'postgres'
   }
 });
 
@@ -26,13 +25,13 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
-app.get('/', (req, res)=> { res.send("it is working") })
+app.get('/', (req, res)=> { res.send(db.users) })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(process.env.PORT || 3000, ()=> {
+app.listen(3000, ()=> {
   console.log('app is running on port 3000');
 })
